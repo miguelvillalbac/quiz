8e39862377a14b31ec0a588a7b6dca7e9e3784f6
miguelvillalbac <mvillalbacastro@gmail.com>
2015-05-18 11:41:41 +0200

    Version con autor

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -11,3 +11,6 @@ exports.answer = function(req, res){
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
 };
+exports.author = function(req, res){
+	res.render('author.ejs', {quiz: req.quiz});
+};
