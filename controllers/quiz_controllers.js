8eb82afa905ea3fee977e2fd0350c8d394b3967b
miguelvillalbac <mvillalbacastro@gmail.com>
2015-05-19 18:55:23 +0200

    Buscador de Preguntas

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -1,27 +1,43 @@
 var models = require('../models/models.js');
+// GET /quizes
 
-exports.question = function(req, res){
-	models.Quiz.findAll().then(function(quiz){
 
-		res.render('quizes/question', { pregunta: quiz[0].pregunta
+exports.index = function(req, res){
+	if(req.query.search){
+		models.Quiz.findAll({where: ["pregunta like ?", "%"+req.query.search.replace(" ","%")+"%"]}).then(
+			function(quizes){
+				res.render('quizes/index.ejs', {quizes: quizes});
+			}
+		).catch(function(error){next(error);});
+	}else{
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index.ejs',{quizes:quizes});
+		}).catch(function(error){next(error);});
+	}
+};
+
+
+exports.show = function(req, res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+
+		res.render('quizes/show', { quiz: quiz});
 
 		})
-	})
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
-
-};
+	};
+	
 
 exports.answer = function(req, res){
-	models.Quiz.findAll().then(function(quiz){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
 
 
-	if(req.query.respuesta == 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
+	if(req.query.respuesta === quiz.respuesta){
+		res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
 	}else{
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
-})
+		res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
+		}
+	})
 };
+
 exports.author = function(req, res){
 	res.render('author.ejs', {quiz: req.quiz});
 };
