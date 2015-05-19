ddb6736e986e0e37000d79809593373cb8f8a0e7
miguelvillalbac <mvillalbacastro@gmail.com>
2015-05-19 12:49:12 +0200

    Base de Datos

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -1,16 +1,28 @@
+var models = require('../models/models.js');
+
 exports.question = function(req, res){
+	models.Quiz.findAll().then(function(quiz){
+
+		res.render('quizes/question', { pregunta: quiz[0].pregunta
+
+		})
+	})
 	res.render('quizes/question', {pregunta: 'Capital de Italia'});
 
 };
 
 exports.answer = function(req, res){
+	models.Quiz.findAll().then(function(quiz){
+
 
 	if(req.query.respuesta == 'Roma'){
 		res.render('quizes/answer', {respuesta: 'Correcto'});
 	}else{
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
+})
 };
 exports.author = function(req, res){
 	res.render('author.ejs', {quiz: req.quiz});
 };
+
